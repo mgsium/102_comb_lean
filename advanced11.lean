e1fd8e089cc3dc8e12f603f2a901aaf6a2a3c686
Kit <liurjk@gmail.com>
2023-10-20 15:23:30 +0100

    Commented and cleaned up theorems

--- advanced11.lean
+++ advanced11.lean
@@ -3,12 +3,11 @@ import Mathlib.Tactic
 open Set
 open Finset
 
+-- x divides an odd number of elements of B
+def oddDivides (x : ℕ) (B : Finset ℕ) := ¬(card (B.filter (fun y ↦ x∣y)) ∣ 2)  
 
-def oddDivides (x : ℕ) (B : Finset ℕ) := ¬(card (B.filter (fun y ↦ x∣y))∣2)  
-  --card {b∈B | divides x b} % 2 = 1
-
-theorem advanced11 (X A : Finset ℕ) (B : Finset ℕ)
-  (h₁ : A ⊆ X) (h₂ : B ⊆ X)(z : 0∉X)
+-- If A is a subset of a finite set X⊆ℤ⁺, then there exists a subset B⊆X such that A is the set of elements of X that divide an odd number of elements of B.
+theorem advanced11 (X A B: Finset ℕ) (h₁ : A ⊆ X) (h₂ : B ⊆ X) (z : 0∉X)
   : ∃B,(A = {x∈X | oddDivides x B}) := by
   sorry
   done
