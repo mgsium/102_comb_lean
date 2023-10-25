412534c3387a0270f0a6f660629d422906b9840a
dodoturkoz <dogukanturkoz14@gmail.com>
2023-10-25 14:23:21 +0100

    disjoint_sets

--- adv22.lean
+++ adv22.lean
@@ -1,10 +1,48 @@
 import Mathlib
 
-open Finset Nat 
+open Finset Nat
+
+--[IMO Shortlist 1994] A subset M of {1,2,3,...,15} does not contain three elements whose product is a perfect square. Determine the maximum number of elements in M.
 
 def is_perfect_square (n : ℕ) : Prop :=
   ∃ m : ℕ, m^2 = n
 
+def is_bad (i j k : ℕ) : Prop :=
+  is_perfect_square (i * j * k)
+
+example : is_bad 2 3 6 := by
+  use 6
+  norm_num
+  done
+
+def set1 : Set ℕ := {1, 4, 9}
+def set2 : Set ℕ := {2, 6, 12}
+def set3 : Set ℕ := {3, 5, 15}
+def set4 : Set ℕ := {7, 8, 14}
+
+theorem disjoint_sets (a b : Set ℕ) (h : Disjoint a b) : ∀ x, x ∈ a → x ∉ b := by
+
+  intro x hx
+  sorry
+  done
+
+
+
+example : Disjoint set1 set2 := by
+  unfold Disjoint
+  intro x hx
+  simp at *
+  intro h
+  apply?
+  sorry
+  done
+
+example : Disjoint set1 set2 ∧ Disjoint set1 set3 ∧ Disjoint set1 set4 ∧ Disjoint set2 set3 ∧ Disjoint set2 set4 ∧ Disjoint set3 set4 := by
+  simp [set1, set2, set3, set4]
+  sorry
+  done
+
+-- if k=15, in general we can take ceil(√k)-1 instead of 3
 example {n : ℕ} : n ^ 2 ≠ 15 := by
   have hn := le_or_lt n 3
   obtain hn' | hn' := hn
@@ -21,14 +59,14 @@ example {n : ℕ} : n ^ 2 ≠ 15 := by
     done
 
 
-def my_set : Finset ℕ := range 16 \ singleton 0 
+def my_set : Finset ℕ := range 16 \ singleton 0
 
 def no_three_square_product (s : Finset ℕ) : Prop :=
   ∀ x y z : s, x ≠ y ∧ y ≠ z ∧ z ≠ x → ¬is_perfect_square (x * y * z)
 
-theorem small_elements : 
+theorem small_elements :
   ∃ s : Finset ℕ, s ⊆ my_set ∧ no_three_square_product s := by
-    use {2, 4, 5}
+    use {2, 3, 4, 5}
     simp
     unfold no_three_square_product
     simp
@@ -36,7 +74,7 @@ theorem small_elements :
     sorry
     done
 
--- theorem ten_elements : 
+-- theorem ten_elements :
 --   ∃ s : Finset ℕ, s ⊆ my_set ∧ no_three_square_product s ∧ s.card = 10 := by
 --     use {1, 4, 5, 6, 7, 10, 11, 12, 13, 14}
 --     simp
@@ -44,7 +82,7 @@ theorem small_elements :
 --     by_cases h1 :
 --     done
 
-theorem max_elements_with_condition : 
+theorem max_elements_with_condition :
   ∀ s : Finset ℕ, s ⊆ my_set → (no_three_square_product s → max s.card = 10) := by
     sorry
     done
