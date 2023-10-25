0d93eaa6e4a1e9827ad0da2a86e02246c05e9a61
Kit <liurjk@gmail.com>
2023-10-25 12:31:49 +0100

    Added calc_Bᵢ_mono

--- advanced11.lean
+++ advanced11.lean
@@ -16,7 +16,7 @@ example : odd_divides 2 { 1, 2, 3 } := by
   done
 
 example (i k : ℕ) (h : i ≤ k) : i - 1 ≤ k := by
-  { norm_num; exact Nat.le_step h }
+  { norm_num; exact le_step h }
 
 def calc_Xᵢ (i : ℕ) (X : Finset ℕ) : Finset ℕ :=
   List.toFinset $ List.take i $ X.sort (· ≥ ·)
@@ -42,7 +42,7 @@ lemma finset_subset (X Y: List ℕ) (h : X ⊆ Y)
   done
 
 lemma calc_X_inc (X : Finset ℕ) (i : ℕ)
-  : calc_Xᵢ i X ⊆ calc_Xᵢ (Nat.succ i) X := by
+  : calc_Xᵢ i X ⊆ calc_Xᵢ (i+1) X := by
   simp [calc_Xᵢ, list_take_subset, finset_subset]
   done
 
@@ -58,7 +58,6 @@ def calc_Bᵢ (i : ℕ) (X A : Finset ℕ) : Finset ℕ := match i with
     let c := Or (And (xₖ ∈ A) (odd_divides xₖ Bₖ)) (And (xₖ ∉ A) ¬(odd_divides xₖ Bₖ))
     Bₖ ∪ (if (¬ c) then { xₖ } else ∅)
 
-
 #eval calc_Xᵢ 3 { 1, 2, 3 }
 #eval calc_Aᵢ 1 { 1, 2, 3 } { 1, 2 }
 #eval calc_Aᵢ 2 { 1, 2, 3 } { 1, 2 }
@@ -67,13 +66,12 @@ def calc_Bᵢ (i : ℕ) (X A : Finset ℕ) : Finset ℕ := match i with
 #eval calc_Bᵢ 2 { 1, 2, 3 } { 1, 2 }
 #eval calc_Bᵢ 3 { 1, 2, 3 } { 1, 2 }
 
-
-
-def adv_11_ith_step (X A B : Finset ℕ) (h₁ : A ⊆ X) (h₂ : B ⊆ X) (z : 0 ∉ X) (i : ℕ) :=
+def adv_11_ith_step (X A : Finset ℕ) (i : ℕ) :=
   let Aᵢ := calc_Aᵢ i X A
   let Bᵢ := calc_Bᵢ i X A
   let Xᵢ := calc_Xᵢ i X
   Aᵢ = { x ∈ Xᵢ | odd_divides x Bᵢ }
+
 
 lemma calc_Bᵢ_zero (X A : Finset ℕ)
   : calc_Bᵢ 0 X A = ∅ := rfl
@@ -93,53 +91,9 @@ lemma calc_Bᵢ_geq_card_X (X A : Finset ℕ)
     intro x
     unfold calc_Bᵢ
     . sorry
-  /-
-  . unfold calc_Bᵢ
-    split
-    . norm_num
-    case _ c
-    . split_ifs
-      case _ d
-      . intro h₁
-        unfold calc_Bᵢ at h₁
-
-      . sorry
-
-
-  . unfold calc_Bᵢ
-    split
-    . norm_num
-    case _ c
-    . sorry
-  -/
+  sorry
   done
 
-lemma calc_Bᵢ_mono3 (X A : Finset ℕ) (i : ℕ)
-  : calc_Bᵢ i X A ⊆ calc_Bᵢ (i + 1) X A := by
-  intro x hₓ
-  match i with
-  | 0 =>
-    simp
-    rw [calc_Bᵢ_zero] at hₓ
-    contradiction
-  | k =>
-    if h : X.card < k then
-      unfold calc_Bᵢ
-      split_ifs
-      case _ c
-      . simp at c
-
-      case _ c
-      . simp at c
-        linarith
-    else
-  simp at h
-  unfold calc_Bᵢ at *
-  split_ifs
-  case _ d
-  . sorry
-  . sorry
-  done
 
 def nat_num_set (i: ℕ) : Finset ℕ := match i with
   | 0 => { 0 }
@@ -158,62 +112,51 @@ lemma nat_num_mono (i: ℕ) : nat_num_set (i) ⊆ nat_num_set (i + 1) := by
     exact h
   done
 
+lemma calc_Bᵢ_threshold (X A : Finset ℕ) (i: ℕ) (h: X.card < i)
+  : calc_Bᵢ i X A = calc_Bᵢ (i + 1) X A := by
+  unfold calc_Bᵢ
+  split
+  . contradiction
+  . split_ifs with a b
+    . rfl
+    . simp at h b
+      by_contra
+      have : card X < card X := lt_trans h b
+      exact LT.lt.false this
+    . contradiction
+    . contradiction
+  done
 
 lemma calc_Bᵢ_mono (X A : Finset ℕ) (i : ℕ)
   : calc_Bᵢ i X A ⊆ calc_Bᵢ (i + 1) X A := by
-  intro x hₓ
-  unfold calc_Bᵢ
-  split_ifs
-  case _ c
-  · unfold calc_Bᵢ
-    split
-    case _ a b
+  by_cases (i = X.card)
+  . intro x hₓ
+    unfold calc_Bᵢ
+    simp at *
+    split_ifs
+    . rw [← h]
+      exact hₓ
     . simp
-      unfold calc_Bᵢ at hₓ
-      split at hₓ
-      case _ c
-      . contradiction
-      case _ c
-      . split at hₓ
-        . rw [b] at hₓ
-          rw [calc_Bᵢ_zero] at hₓ
-          contradiction
-        case _ d e f
-        . refine not_succ_le_zero ?_ ?_
-          . exact x
-          . simp
-            rw [b] at f
-            rw [not_lt] at f
-            contradiction
-    case _ c
-    . split_ifs
-      case _ d
-      .
-      . sorry
-  . simp
-    apply Or.inl
-    exact hₓ
+      exact hₓ
+    . simp
+      apply Or.inl
+      exact hₓ
+  . by_cases (i > X.card)
+    . refine subset_of_eq ?_
+      apply calc_Bᵢ_threshold X A i
+      exact h
+    . simp at h
+      intro x hₓ
+      unfold calc_Bᵢ
+      split_ifs with a <;> simp at a
+      case _ hₙ
+      . by_contra
+        have : i < card X := lt_of_le_of_ne h hₙ
+        apply le_lt_antisymm this a
+      . simp
+        apply Or.inl
+        exact hₓ
   done
-
-
-lemma calc_Bᵢ_mono2 (X A : Finset ℕ) (i : ℕ)
-  : calc_Bᵢ i X A ⊆ calc_Bᵢ (i + 1) X A := by
-  intro x hₓ
-  unfold calc_Bᵢ
-  split_ifs
-  case _ c
-  . unfold calc_Bᵢ at hₓ
-    split at hₓ
-    case _ d
-    . contradiction
-    . split_ifs at hₓ
-      . exact hₓ
-      case _ _ f
-      sorry
-  . sorry
-  done
-
-
 
 
 
--- test.lean
+++
@@ -1,9 +0,0 @@
-import Mathlib.Tactic
-
-#check Mul.mul
-
-lemma h : 1 + 0 = 1 := rfl
-
-example : 2 + 0 = 2 := by
-  exact rfl
-  done
