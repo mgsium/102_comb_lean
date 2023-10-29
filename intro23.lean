fe84b7f5b1c5ad95b40764350127699b336aa5b3
Kit <30439030+DesyncTheThird@users.noreply.github.com>
2023-10-29 18:07:10 +0000

    Work on theorem statement

--- intro23.lean
+++ intro23.lean
@@ -1,12 +1,12 @@
 import Mathlib.Tactic
 
-
+open List
 open Set
 open Finset
 open Nat
 
 def tri_ineq (a b c : ℕ) : Bool :=
-  a + b < c
+  a + b > c
 
 lemma tri_ineq_symm (a b c : ℕ) : tri_ineq a b c = tri_ineq b a c := by
   unfold tri_ineq
@@ -14,19 +14,33 @@ lemma tri_ineq_symm (a b c : ℕ) : tri_ineq a b c = tri_ineq b a c := by
   done
 
 def tri_prop (X : Finset ℕ) : Bool :=
-  ∃ a b c : X,
-   (tri_ineq a b c
+  ∃ a b c : X, a≠b ∧ b≠c ∧ c≠a
+  ∧ tri_ineq a b c
   ∧ tri_ineq b c a
-  ∧ tri_ineq c a b)
+  ∧ tri_ineq c a b
 
-
-def range_set (n : ℕ) (h : n ≥ 4) : Finset ℕ :=
-  (Finset.range n) \ {0,1,2,3}
+def four_to_n (n : ℕ) (h : n ≥ 4) : Finset ℕ :=
+  (Finset.range (n+1)) \ {0,1,2,3}
   -- {i : ℕ | 4≤i ∧ i≤n}'(by linarith)
 
+def finset_max (S : Finset ℕ) : ℕ :=
+  match Finset.max S with
+  | some a => a
+  | none => 0
+
+-- n; {4,5,6,...,n}
+-- c; cardinality of subsets
+def tri_prop_c_subsets (n c : ℕ) (h₀ : n≥4) : Finset (Finset ℕ) :=
+  filter (fun X => (tri_prop X) ∧ (card X = c)) (four_to_n n h₀).powerset
+
+def all_c_subsets (n c : ℕ) (h₀ : n≥4) : Finset (Finset ℕ) :=
+  filter (fun X => card X = c) (four_to_n n h₀).powerset
+
+#eval tri_prop_c_subsets 15 3 (by norm_num)
 
 theorem intro23 (n : ℕ) (h₀ : n≥4) (X : Finset ℕ)
-  : List.maximum {n : (∀X, X⊆range_set n h₀ ∧ card X = 10 ∧ tri_prop X)} = 253
-  := by
-
+  : Finset.max {i | (tri_prop_c_subsets n 10 h₀) = (all_c_subsets n 10 h₀)} = 253 := by
+  sorry
   done
+
+--theorem intro23_254counterexample :
