73a9c59d8058c43d0dd79606e0161a9574b1bd78
Kit <30439030+DesyncTheThird@users.noreply.github.com>
2023-10-29 20:30:32 +0000

    Main proof complete

--- intro23.lean
+++ intro23.lean
@@ -1,46 +1,93 @@
 import Mathlib.Tactic
 
-open List
-open Set
-open Finset
-open Nat
+/-!
+# Intro 23 (p.34) | [AIME 2001]
 
-def tri_ineq (a b c : ℕ) : Bool :=
+A set of positive numbers has the _triangle property_ if it has three
+distinct elements that are the lengths of the sides of a triangle whose area
+is positive. Consider sets {4,5,6,...,n} of consecutive positive integers, all
+of whose ten-element subsets have the triangle property. What is the largest
+possible value of n?
+
+-/
+
+open Finset Nat List
+
+--------------------------------------------------------------------------------
+---| SETUP |--------------------------------------------------------------------
+--------------------------------------------------------------------------------
+section setup
+
+def tri_ineq (a b c : ℕ) : Prop :=
   a + b > c
 
-lemma tri_ineq_symm (a b c : ℕ) : tri_ineq a b c = tri_ineq b a c := by
-  unfold tri_ineq
-  rw [add_comm]
-  done
-
-def tri_prop (X : Finset ℕ) : Bool :=
+-- a, b, and c satisfy tri_ineq in all permutations
+def tri_prop (X : Finset ℕ) : Prop :=
   ∃ a b c : X, a≠b ∧ b≠c ∧ c≠a
   ∧ tri_ineq a b c
   ∧ tri_ineq b c a
   ∧ tri_ineq c a b
 
-def four_to_n (n : ℕ) (h : n ≥ 4) : Finset ℕ :=
-  (Finset.range (n+1)) \ {0,1,2,3}
-  -- {i : ℕ | 4≤i ∧ i≤n}'(by linarith)
-
-def finset_max (S : Finset ℕ) : ℕ :=
-  match Finset.max S with
-  | some a => a
-  | none => 0
-
 -- n; {4,5,6,...,n}
 -- c; cardinality of subsets
-def tri_prop_c_subsets (n c : ℕ) (h₀ : n≥4) : Finset (Finset ℕ) :=
-  filter (fun X => (tri_prop X) ∧ (card X = c)) (four_to_n n h₀).powerset
+def all_c_subsets_satisfy_tri_prop (c n : ℕ) : Prop :=
+  ∀(X : Finset ℕ), (X ⊆ Icc 4 n) ∧ (card X = c) → tri_prop X
 
-def all_c_subsets (n c : ℕ) (h₀ : n≥4) : Finset (Finset ℕ) :=
-  filter (fun X => card X = c) (four_to_n n h₀).powerset
+end setup
+--------------------------------------------------------------------------------
+---| USEFUL LEMMAS |------------------------------------------------------------
+--------------------------------------------------------------------------------
+section useful_lemmas
 
-#eval tri_prop_c_subsets 15 3 (by norm_num)
-
-theorem intro23 (n : ℕ) (h₀ : n≥4) (X : Finset ℕ)
-  : Finset.max {i | (tri_prop_c_subsets n 10 h₀) = (all_c_subsets n 10 h₀)} = 253 := by
-  sorry
+-- [4,n] ⊆ [4,n+1]
+lemma four_to_n_subset {n : ℕ} : Icc 4 n ⊆ Icc 4 (n+1) := by
+  intro x h
+  simp only [gt_iff_lt, mem_Icc] at *
+  constructor <;> linarith
   done
 
---theorem intro23_254counterexample :
+-- Not all 10-element subsets satisfy tri_prop for n = 254.
+lemma intro23_254counterexample
+    : ¬all_c_subsets_satisfy_tri_prop 10 254 := by
+  let Y : Finset ℕ := {4, 5, 9, 14, 23, 37, 60, 97, 157, 254}
+  have h₁ : Y ⊆ Icc 4 254 := by
+    simp_all only [mem_insert, Finset.mem_singleton, gt_iff_lt]
+  have h₂ : card Y = 10 := by rfl
+  have h₃ : ¬tri_prop Y := by exact Bool.not_iff_not.mp rfl
+  unfold all_c_subsets_satisfy_tri_prop
+  push_neg
+  use Y
+  done
+
+-- A counterexample for n = k is also a counterexample for n = k + 1.
+lemma counterexample_upwards (c k : ℕ)
+    : ¬all_c_subsets_satisfy_tri_prop c k
+    → ¬all_c_subsets_satisfy_tri_prop c (k+1) := by
+  unfold all_c_subsets_satisfy_tri_prop
+  push_neg
+  choose Y h
+  use Y
+  repeat (any_goals constructor)
+  . apply Finset.Subset.trans h.1.1
+    exact four_to_n_subset
+  . exact h.1.2
+  . exact h.2
+  done
+
+end useful_lemmas
+--------------------------------------------------------------------------------
+---| MAIN THEOREM |-------------------------------------------------------------
+--------------------------------------------------------------------------------
+
+theorem intro23 (n : ℕ)
+    : n ≥ 254 → ¬all_c_subsets_satisfy_tri_prop 10 n := by
+  intro g
+  induction' n with d hd
+  . contradiction
+  . by_cases 254 = succ d
+    . rw [← h]
+      exact intro23_254counterexample
+    . have : d ≥ 254 := lt_succ.mp $ Nat.lt_of_le_of_ne g h
+      apply (counterexample_upwards 10 d)
+      exact hd this
+  done
