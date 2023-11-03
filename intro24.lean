b8125ce4cdbfe0caa2afde9177eb89af919d65c4
Kit <30439030+DesyncTheThird@users.noreply.github.com>
2023-11-03 17:59:29 +0000

    Elements must be distinct

--- intro24.lean
+++ intro24.lean
@@ -8,7 +8,7 @@ def disjoint_nat_union (A B : Set ℕ) : Prop :=
 
 theorem intro24 (n a b: ℕ) (A B : Set ℕ) :
   disjoint_nat_union A B
-  → ∀n, ∃ a b, a>n ∧ b>n
+  → ∀n, ∃ a b, a≠b ∧ a>n ∧ b>n
   ∧ ({a, b, a+b} ⊆ A ∨ {a, b, a+b} ⊆ B) := by
   sorry
   done
