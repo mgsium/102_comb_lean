473fe5a20134f2a3a8bf2ab992aac84594a93ccf
dodoturkoz <dogukanturkoz14@gmail.com>
2023-11-09 13:27:16 +0000

    added (with some proofs) parity lemmas for sums

--- intro3.lean
+++ intro3.lean
@@ -6,10 +6,6 @@ open Nat
 open BigOperators
 open Finset
 
--- def n : ℕ := 11
-
-def my_set : Finset ℕ := range (1+(n-1)/2) \ singleton 0
-
 lemma aux1 : ∑ k in range (n+1), choose n k = 2^n := by
   exact sum_range_choose n
   done
@@ -19,6 +15,10 @@ def my_set2 : Finset ℕ := range n \ singleton 0
 lemma aux1' : ∑ k in my_set2 n, choose n k = 2^n-2 := by
     sorry
     done
+
+-- def n : ℕ := 11
+
+def my_set : Finset ℕ := range (1+(n-1)/2) \ singleton 0
 
 lemma aux2 (h:n > 1)(h': Odd n): 2 * ∑ k in my_set n, choose n k = 2^n-2 := by
   simp [my_set]
@@ -45,37 +45,76 @@ theorem odd_number_of_odd_numbers {n : ℕ} (h : Odd n) :
   done
 
 
-#check sum_filter_add_sum_filter_not
-
 theorem odd_filter (set' : Finset ℕ) :
    ∑ k in set', k = ∑ k in (set'.filter (λ x => Odd x)), k
    +  ∑ k in (set'.filter (λ x => ¬ Odd x)), k   := by
    exact (sum_filter_add_sum_filter_not set' (fun x => Odd x) fun x => x).symm
    done
 
-#check even_add
+lemma odd_sum {n : ℕ} (X : Finset ℕ) (h': card X = 2*n): Even (∑ k in X, k) := by
+  unfold Even
+  induction' n with d hd
+  .simp [card_eq_zero.mp h']
+   use 0
+   rfl
+  . sorry
+  done
 
-lemma odd_sum (set' : Finset ℕ) (h: ¬Odd (card (filter (fun x => Odd x) set'))):
+lemma filter_odd_sum (set' : Finset ℕ) (h: ¬Odd (card (filter (fun x => Odd x) set'))):
  Even (∑ k in filter (fun x => Odd x) set', k) := by
-  simp
+  rw [even_iff_not_odd.symm] at h
   sorry
+  simp at h
+  exact h
   done
 
-lemma evensss_sum (X : Finset ℕ )(h: ∀ x ∈ X, Even x): Even (∑ k in X, k) := by
+lemma even_sum {n : ℕ} (X : Finset ℕ) (h': card X = n)(h: ∀ x ∈ X, Even x):
+  Even (∑ k in X, k) := by
   unfold Even
-  sorry
+  induction' n with d hd
+  .simp [card_eq_zero.mp h']
+   use 0
+   rfl
+  . obtain ⟨x, hx⟩ : ∃ x, x ∈ X := card_pos.mp (h'.symm ▸ succ_pos d)
+    let X' := erase X x
+    have hX' : card X' = d := (card_erase_of_mem hx).trans (congrArg pred h')
+    have h'' : Even (∑ k in X', k) := sorry
+    obtain ⟨k, hk⟩ := h x hx
+    have h''' : Even (k + ∑ k in X', k) := by
+      have he: Even x := by sorry
+      apply Even.add he h''
+      done
+    sorry
+  done
+
+theorem test (x k: ℕ)(h: x = k + k): (Even x) := by
+  rw [h] at *
+  exact even_add_self k
+  done
+
+theorem extracted_1 {n : ℕ} (X : Finset ℕ) (h'_1 : card X = n) (h : ∀ (x : ℕ), x ∈ X → Even x) (d : ℕ)
+  (hd : card X = d → ∃ r, ∑ k in X, k = r + r) (h' : card X = succ d) : ∃ r, ∑ k in X, k = r + r := sorry
+
+lemma filter_even_sum (set' : Finset ℕ):
+  Even (∑ k in filter (fun x => ¬Odd x) set', k) := by
+  let even_set := filter (fun x => ¬Odd x) set'
+  have h : ∀ x ∈ even_set, Even x := by
+    intros x hx
+    simp at hx
+    obtain ⟨_,h2⟩ := hx
+    exact h2
+  apply (even_sum even_set rfl h)
   done
 
 
-lemma even_sum (set' : Finset ℕ): Even (∑ k in filter (fun x => ¬Odd x) set', k) := by
-  simp
-  unfold Even
-  sorry
-  done
-
-theorem general_odd {n : ℕ} (set' : Finset ℕ) (h : Odd (∑ k in set', k)) :
+theorem general_odd (set' : Finset ℕ) (h : Odd (∑ k in set', k)) :
   Odd (card (set'.filter (λ x => Odd x))) := by
   contrapose h
+  simp
   rw [odd_filter]
-  sorry
+  have h1: Even (∑ k in filter (fun x => ¬Odd x) set', k)
+    := by exact filter_even_sum set'
+  have h2: Even (∑ k in filter (fun x => Odd x) set', k)
+    := by exact filter_odd_sum set' h
+  apply Even.add h2 h1
   done
