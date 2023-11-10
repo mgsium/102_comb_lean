f47674f7ab8e136f4d01539547b39637344ac1d4
Kit <liurjk@gmail.com>
2023-11-10 15:04:59 +0000

    wip

--- intro38.lean
+++ intro38.lean
@@ -23,9 +23,13 @@ is even. Then, for
 
 open BigOperators
 
-theorem even_sum2 {n : ℕ} (X : Finset ℕ) (h : Even $ card X) : Even (∑ x in X, x) := by
-  unfold Even
-  apply?
+example (n : ℕ) (h : n≥2) : (2*n-(1:ℚ) ) % 2 = 1 := by
+  push_cast
+
+  unfold Odd
+  use n-1
+  norm_num
+  ring_nf
   done
 
 
@@ -34,13 +38,12 @@ instance : Membership ℕ (Finset ℕ) := by
   sorry
   done
 
-
 noncomputable
 instance {V : Finset ℕ} {G : SimpleGraph V} {v : V}
   : Fintype (SimpleGraph.neighborSet G v) :=
     Fintype.ofFinite ↑(SimpleGraph.neighborSet G v)
 
-theorem intro38 {V : Finset ℕ} (n : ℕ) (G : SimpleGraph V) (hₓ : n≥2)
+theorem intro38 {V : Finset ℕ} (n : ℕ) (G : SimpleGraph V) (h₁ : n≥1)
   (h : ∀(v:V), G.degree v % 2 = 0) (c : V.card = 2 * n)
   : ∃(a b : V), (card (G.neighborFinset a ∩ G.neighborFinset b) % 2 = 0) := by
   by_contra g
@@ -49,8 +52,12 @@ theorem intro38 {V : Finset ℕ} (n : ℕ) (G : SimpleGraph V) (hₓ : n≥2)
   have h₁ : ∀(p : V), (card (V.attach.erase p)) % 2 = 1 := by
     simp
     intro a H
-    rw [c]
-
+    rw [c, ← Nat.odd_iff]
+    unfold Odd
+    use n-1
+    match n with
+    | 0   => contradiction
+    | k+1 => rw [Nat.mul_add]; simp
     done
   have h₂ : ∀(p : V), (card (G.neighborFinset p)) % 2 = 0 := by
     sorry
