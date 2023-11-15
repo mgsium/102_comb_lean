0156b6213e3503ca048818a17dfc8159dbdb8a5b
Musab Guma'a <musabgumaa@gmail.com>
2023-11-15 18:01:29 +0000

    started to prove even_sum_of_odd

--- new-intro3.lean
+++ new-intro3.lean
@@ -113,6 +113,24 @@ lemma sum_odd (h: 1 < n): Odd (∑ k in my_set n, choose n k + (0:ℚ)) := by
 
 -- if the sum of the elements of a finset is odd, then the finset contains an odd number of odd numbers.
 
+#check Finset.card_insert_of_not_mem
+
+universe u_2
+
+#check Odd.add_odd
+
+theorem odd_add_one {α : Type u_2} [Semiring α] {m : α} (h: Odd m)
+  : Even (m + 1) := Odd.add_odd h odd_one
+
+theorem odd_iff_even_add_one {m : ℕ}
+  : Odd m ↔ Even (m + 1) := by
+    constructor
+    . exact odd_add_one
+    . contrapose
+      rw [← even_iff_not_odd, ← odd_iff_not_even]
+      exact Even.add_one
+    done
+
 theorem odd_filter (set' : Finset ℕ) :
    ∑ k in set', k = ∑ k in (set'.filter Odd), k
    +  ∑ k in (set'.filter (¬ Odd ·)), k := by
@@ -121,9 +139,6 @@ theorem odd_filter (set' : Finset ℕ) :
 
 def even_sum_finset (X: Finset ℕ) : Prop :=
   (∀ x ∈ X, Even x) → Even (∑ k in X, k)
-
-def odd_sum_finset (X: Finset ℕ): Prop :=
-  (Even $ card X) ∧ (∀ x ∈ X, Odd x) → Even (∑ k in X, k)
 
 lemma even_sum (X : Finset ℕ) :even_sum_finset X := by
   have empty : even_sum_finset ∅ := by
@@ -147,50 +162,45 @@ lemma even_sum (X : Finset ℕ) :even_sum_finset X := by
     simp [even_a]
   done
 
-#check Finset.card_insert_of_not_mem
+def odd_sum_finset (X: Finset ℕ): Prop :=
+  (Even $ card X) ∧ (∀ x ∈ X, Odd x) → Even (∑ k in X, k)
 
-universe u_2
+lemma even_sum_of_odd (X : Finset ℕ) : odd_sum_finset X := by
+  unfold odd_sum_finset Even Odd
+  intro h
 
-#check Odd.add_odd
+  -- have empty : odd_sum_finset ∅ := by
+  --   simp only [odd_sum_finset]
+  --   done
 
-theorem odd_add_one {α : Type u_2} [Semiring α] {m : α} (h: Odd m)
-  : Even (m + 1) := Odd.add_odd h odd_one
+  -- refine @Finset.induction ℕ odd_sum_finset _ empty ?_ X
+  -- intro a X' h g
+  -- unfold odd_sum_finset at *
+  -- intro h'
 
-theorem odd_iff_even_add_one {m : ℕ}
-  : Odd m ↔ Even (m + 1) := by
-    constructor
-    . exact odd_add_one
-    . contrapose
-      rw [← even_iff_not_odd, ← odd_iff_not_even]
-      exact Even.add_one
-    done
+  -- by_contra h'
+  -- push_neg at h'
 
-lemma odd_sum (X : Finset ℕ) : odd_sum_finset X := by
-  have empty : odd_sum_finset ∅ := by
-    simp only [odd_sum_finset]
-    done
-  refine @Finset.induction ℕ odd_sum_finset _ empty ?_ X
-  intro a X' h g
-  unfold odd_sum_finset at *
-  by_contra h'
-  push_neg at h'
-  have odd_card_X' : Odd (card X') := by
-    have ⟨hₗ, _⟩ := h'.1
-    rw [card_insert_of_not_mem h] at hₗ
+  -- have odd_card_X' : Odd (card X') := by
+  --   have ⟨hₗ, _⟩ := h'.1
+  --   rw [card_insert_of_not_mem h] at hₗ
+  --   exact odd_iff_even_add_one.mpr hₗ
+  --   done
 
-    done
 
-  by_cases h': Even (card X')
-  . rw [Finset.card_insert_of_not_mem h, imp_iff_not_or, not_and_or]
-    left; left
-    simp only [even_iff_not_odd, not_not, Even.add_one h']
-  . rw [sum_insert h, even_add]
-    intro h''
-    by_contra
-    have odd_card_X' : Odd (card X') := by
-      rw [odd_iff_not_even]
-      exact h'
-      done
+
+  -- by_cases h': Even (card X')
+  -- . rw [Finset.card_insert_of_not_mem h, imp_iff_not_or, not_and_or]
+  --   left; left
+  --   simp only [even_iff_not_odd, not_not, Even.add_one h']
+  -- . rw [sum_insert h, even_add]
+  --   intro h''
+  --   by_contra
+  --   have odd_card_X' : Odd (card X') := by
+  --     rw [odd_iff_not_even]
+  --     exact h'
+  --     done
+
     -- constructor
     -- . rw [imp_iff_not_or]
     --   left
