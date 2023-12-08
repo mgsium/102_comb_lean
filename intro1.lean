1d5c270b990a7b8ee1ce8d5b1f5be06392a24758
Kit <liurjk@gmail.com>
2023-12-08 19:37:10 +0000

    Done

--- intro1.lean
+++ intro1.lean
@@ -16,7 +16,7 @@ number of monograms is 24+23+22+...+2+1 = 300.
 
 -/
 
-open Nat Set Finset Function Fin
+open Nat Finset Function Fin
 
 --------------------------------------------------------------------------------
 ---| SETUP |--------------------------------------------------------------------
@@ -50,7 +50,7 @@ def between_a_25 (a : Alphabet) : Finset Alphabet :=
   filter (fun b ↦ ordered a b) univ
 
 -- starts_with a → between_a_25
-def set_bijection (a : Alphabet) (p : Alphabet × Alphabet) (h : p ∈ starts_with a) :
+def set_bijection (a : Alphabet) (p : Alphabet × Alphabet) (_ : p ∈ starts_with a) :
   Alphabet := p.2
 
 end setup
@@ -61,87 +61,75 @@ section useful_lemmas
 
 example (a b c : ℕ) (h : a=b) (h' : b≠c) : a≠c := by exact Eq.trans_ne h h'
 
-lemma starts_with_disjoint : PairwiseDisjoint (Finset.univ).toSet starts_with := by
-  unfold PairwiseDisjoint Set.Pairwise onFun starts_with
-  simp_rw [Finset.disjoint_left]
+lemma starts_with_disjoint : Set.PairwiseDisjoint univ.toSet starts_with := by
+  unfold Set.PairwiseDisjoint Set.Pairwise onFun starts_with
+  simp_rw [disjoint_left]
   intro _ _ _ _ g _ ha
   rw [mem_filter] at *
-  simp_rw [Finset.mem_univ, true_and] at *
+  simp_rw [mem_univ, true_and] at *
   apply not_and_of_not_right
   exact Eq.trans_ne ha.2 g
   done
 
-lemma set_eq : Finset.disjiUnion univ starts_with starts_with_disjoint = monograms
-  := by
+lemma set_eq : disjiUnion univ starts_with starts_with_disjoint = monograms := by
   ext x
   unfold monograms starts_with
-  simp_rw [mem_filter, Finset.mem_univ, true_and]
   simp [uncurry_def]
   done
 
-lemma fst_eq_a (a : Alphabet) (p : (Alphabet × Alphabet))
+lemma fst_eq_a (a : Alphabet) (p : Alphabet × Alphabet)
   : p ∈ starts_with a → p.1 = a := by
   unfold starts_with
   rw [mem_filter]
-  simp_rw [Finset.mem_univ, true_and]
+  simp_rw [mem_univ, true_and]
   exact fun h ↦ h.2
   done
 
 lemma icc_between_a_25 (a : Alphabet)
-  : Finset.image (fun x ↦ x.val) (between_a_25 a) = Finset.Ioo a.val 25 := by
+  : image (fun x ↦ x.val) (between_a_25 a) = Ioo a.val 25 := by
   ext b
-  simp only [Finset.mem_image, gt_iff_lt, is_lt, not_true, ge_iff_le, Finset.mem_Ioo]
+  simp only [mem_image, gt_iff_lt, is_lt, not_true, ge_iff_le, mem_Ioo]
   unfold between_a_25
-  simp_rw [mem_filter, Finset.mem_univ, true_and]
+  simp_rw [mem_filter, mem_univ, true_and]
   unfold ordered
   constructor <;> intro h
-  . let ⟨x, ⟨g, g'⟩⟩ := h
+  . let ⟨_, ⟨g, g'⟩⟩ := h
     rw [← g']
     simp only [val_fin_lt]
     exact g
   . use ofNat'' b --(pos a)
     simp only [ofNat_eq_val, coe_ofNat_eq_mod, mod_succ_eq_iff_lt]
-    let ⟨g,g'⟩ := h
-    repeat constructor
-    . rw [lt_iff_val_lt_val, val_cast_of_lt (by linarith)]
-      exact g
-    . exact Nat.lt_of_le_of_lt (Nat.mod_le b 26) g'
-    . linarith
+    refine ⟨⟨?_, lt_of_le_of_lt (mod_le b 26) h.2⟩, (by linarith)⟩
+    rw [lt_iff_val_lt_val, val_cast_of_lt (by linarith)]
+    exact h.1
   done
 
 lemma starts_with_card (a : Alphabet) : card (starts_with a) = 24 - a.val := by
-  rw [@Finset.card_congr _ _ (starts_with a) (between_a_25 a) (set_bijection a)]
-  . rw [← @Finset.card_image_of_injective _ _ (fun x ↦ x.val) _ _ (by simp)]
+  rw [@card_congr _ _ (starts_with a) (between_a_25 a) (set_bijection a)]
+  . rw [← @card_image_of_injective _ _ (fun x ↦ x.val) _ _ (by simp)]
     rw [icc_between_a_25 a]
     simp only [gt_iff_lt, not_lt, ge_iff_le, Nat.card_Ioo, tsub_le_iff_right]
     exact tsub_right_comm
   . intro p hp
-    unfold set_bijection between_a_25
-    unfold starts_with at hp
+    unfold set_bijection between_a_25 starts_with at *
     rw [mem_filter] at *
-    simp_rw [Finset.mem_univ, true_and] at *
+    simp_rw [mem_univ, true_and] at *
     rw [← hp.2]
     exact hp.1
-  . intro p q hp hq
-    push_neg
-    intro h
+  . intro p q hp hq h
     unfold set_bijection at h
     ext
     . rw [fst_eq_a a p hp, fst_eq_a a q hq]
     . exact h
   . intro b h
-    use (a,b)
-    have h' : (a,b) ∈ starts_with a := by
-      unfold starts_with
+    use (a,b), ?_
+    . simp [set_bijection]
+    . unfold starts_with
       rw [mem_filter]
-      simp_rw [Finset.mem_univ, true_and, and_true]
+      simp_rw [mem_univ, true_and, and_true]
       unfold between_a_25 at h
-      simp only [Finset.mem_univ, forall_true_left, mem_filter, true_and] at h
+      simp only [mem_univ, forall_true_left, mem_filter, true_and] at h
       exact h
-      done
-    use h'
-    unfold set_bijection
-    trivial
   done
 
 end useful_lemmas
