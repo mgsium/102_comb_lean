344b423ce3ddc3fa9f66075633e94915c6f5ef8c
Kit <30439030+DesyncTheThird@users.noreply.github.com>
2024-01-08 10:44:46 +0000

    wip

--- intro22.lean
+++ intro22.lean
@@ -1,4 +1,6 @@
 import Mathlib.Tactic
+import Mathlib.Data.Finset.Sym
+import Mathlib.Data.Sym.Sym2
 
 /-!
 # Intro 22 (p.4)
@@ -14,7 +16,6 @@ So if S has n elements, there are 3ⁿ ways to choose A and B. Apart from cases
 with A = B, this counts each pair of sets exactly twice, since we may permute A
 and B. Also, since A ∪ B = S, A = B if and only if A = B = S, so the total
 number of pairs is ((3ⁿ - 1)/2) + 1.
-
 -/
 
 open Finset Function
@@ -62,6 +63,7 @@ def UProd (α : Type u) : Type u :=
 def mk {α : Type} (a₁ a₂ : α) : UProd α :=
   Quotient.mk' (a₁, a₂)
 notation "「" a₁ ", " a₂ "」" => mk a₁ a₂
+--「a,b」for an unordered pair
 
 --(ha : A ⊆ S) (hb : B ⊆ S)
 def union_prop (A B S : Finset ℕ) : Prop := A ∪ B = S
@@ -73,7 +75,6 @@ instance decUnion_prop (S : Finset ℕ) : DecidablePred (uncurry union_prop · S
   exact decEq (A ∪ B) S
   done
 
---「」
 lemma swap_eq (a b : Finset ℕ) : 「a, b」 = 「b, a」 := by
   refine Quot.sound ?_
   unfold Setoid.r
@@ -99,12 +100,24 @@ instance decUpair_Union_prop (S : Finset ℕ) : DecidablePred (uncurry upair_uni
 def subset_pairs (S : Finset ℕ) : (Finset $ Finset ℕ × Finset ℕ) :=
   filter (uncurry union_prop · S) (Finset.product (Finset.powerset S) (Finset.powerset S))
 
-instance psetSetoid (P : Finset ℕ × Finset ℕ) : Setoid P :=
-   ⟨swapProp, swapEquiv⟩
+private def swapPropN (p₁ p₂ : Finset ℕ × Finset ℕ) : Prop :=
+  (p₁.1 = p₂.1 ∧ p₁.2 = p₂.2) ∨ (p₁.1 = p₂.2 ∧ p₁.2 = p₂.1)
+notation:50 lhs:51 " ~ " rhs:51 => swapProp lhs rhs
 
-def subset_upairs (P : Finset $ Finset ℕ × Finset ℕ) : (Finset $ UProd $ Finset ℕ) :=
-  Quotient psetSetoid
-  done
+instance uprodSetoidN : Setoid (Finset ℕ × Finset ℕ) :=
+  ⟨swapPropN, swapEquiv⟩
+
+def subset_upairs (S : Finset ℕ) : Finset (Finset ℕ × Finset ℕ) :=
+  --Quotient (uprodSetoid $ Finset ℕ)
+  -- filter (x ↦ Quotient.mk' (uncurry union_prop · S) x) (Finset.product (Finset.powerset S) (Finset.powerset S))
+  --Quotient.mk' (subset_pairs S)
+  --filter (uncurry union_prop · S) (Finset.powerset S).sym2
+  Sym2.mk $ subset_pairs S
+
+
+#check subset_pairs {1,2,3}
+#check uprodSetoid $ Finset ℕ
+#check Quotient (uprodSetoid $ Finset ℕ)
 
 end setup
 --------------------------------------------------------------------------------
@@ -117,11 +130,13 @@ end useful_lemmas
 ---| MAIN THEOREM |-------------------------------------------------------------
 --------------------------------------------------------------------------------
 
+#eval subset_pairs {1,2,3}
+
 set_option maxRecDepth 8000
 
--- theorem test : card (subset_pairs {1,2}) = 9 := by
---   decide
---   done
+theorem test : card (subset_pairs {1,2}) = 9 := by
+   decide
+   done
 
 -- theorem test2 : card (subset_pairs {1,2,3}) = 27 := by
 --   decide
